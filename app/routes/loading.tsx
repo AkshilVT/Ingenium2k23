79ea90edb3e6f812dea948e62b14b29cd2177c0e
heet9201 <heetdedakiya80@gmail.com>
2022-11-13 05:47:53 +0530

    loading animation

---
+++ app/routes/loading.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+
+function loading() {
+  return (
+      <div className='h-screen bg-black'>
+        {/* <div className='h-screen grid place-content-center'> */}
+        <div className='h-screen flex justify-center items-center'>
+            <Player
+  autoplay
+  loop
+  src="https://assets7.lottiefiles.com/packages/lf20_t9gkkhz4.json"
+  style={{ height: '300px', width: '300px' }}
+>
+  {/* <Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} /> */}
+</Player>
+        </div>
+      </div>
+  )
+}
+
+export default loading
