c2a5c963516b7ee079c372a5d2c5b7312cac2535
malay44 <malay.p1@ahduni.edu.in>
2022-11-14 13:16:15 +0530

    glass effect added to events section

--- app/routes/events.tsx
+++ app/routes/events.tsx
@@ -9,10 +9,10 @@ function events() {
       <div className="grid gap-4 gap-y-9 lg:grid-cols-3 md:grid-cols-2 justify-items-center max-w-[1200px] my-0 mx-auto px-4">
         {arr.map((item) => (
             <div className="p-6 max-w-[400px] min-h-[540px] aspect-[9/11]" key={item}>
-              <div className="h-full group rounded-xl bg-cover bg-center bg-[url('https://tvseriescritic.files.wordpress.com/2016/10/stranger-things-bicycle-lights-children.jpg')]">
-                <div className="data text-white p-3 flex items-end h-full ">
-                  <div className="content translate-y-[116px] group-hover:translate-y-0  duration-300 ease-linear">
-                    <span className="author">Jane Doe</span>
+              <div className="h-full group rounded-xl bg-cover bg-center bg-[url('public/poster.jpg')]">
+                <div className="data text-white p-3 flex items-end h-full overflow-hidden ">
+                  <div className="content rounded-xl translate-y-[116px] group-hover:translate-y-0 duration-300 ease-linear backdrop-blur">
+                    <span className="author py-3">Jane Doe</span>
                     <h1 className="title text-3xl font-[200] py-2">Stranger Things: The sound of the Upside Down</h1>
                     <div className="opacity-0 group-hover:opacity-100 duration-300 ease-linear">
                       <p className="text font-ligh text-sm py-2">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
