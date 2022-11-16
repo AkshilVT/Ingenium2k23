04fa65d08ebd678aa318c774308e7ffe1b8ad227
malay44 <malay.p1@ahduni.edu.in>
2022-11-16 22:47:31 +0530

    fine tuned event cards page

---
+++ app/routes/eventDetail.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "@remix-run/react"
+import styles from "./styles/events.css";
+
+export default () => {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    );
+}
