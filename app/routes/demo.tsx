8d5b9f06a98ba01f29030544bc3baf468cc30122
heet9201 <heetdedakiya80@gmail.com>
2022-12-10 01:26:43 +0530

    tsparticles added

---
+++ app/routes/demo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ParticleBackground from "../../components/particles/ParticleBackgorund";
+const demo = () => {
+  return <ParticleBackground />;
+};
+
+export default demo;
